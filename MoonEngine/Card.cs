d0fa0e3d16844fa411f3c63340b787db60f58ca1
superqjx <superqjx@hotmail.com>
2016-08-24 18:54:47 +0800

    nothing

---
+++ MoonEngine/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonEngine
+{
+    class Card
+    {
+        private string name;
+        public string Name { get; set; } 
+        private Card()
+        {
+            //TODO;
+        }
+        public Card(string def)
+        {
+            //TODO;
+        }
+    }
+}
