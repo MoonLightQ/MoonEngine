d4d6d1691239ac688a8a0e47bdf6a586cb83761d
superqjx <superqjx@hotmail.com>
2016-08-24 19:29:49 +0800

    Card 类一点微小的工作。

--- MoonEngine/Card.cs
+++ MoonEngine/Card.cs
@@ -8,15 +8,33 @@ namespace MoonEngine
 {
     class Card
     {
-        private string name;
-        public string Name { get; set; } 
+        public readonly UInt32 ID;
+        public readonly string Name;
+        public readonly string Description;
+
+        private readonly Int32 originAttack;
+        private readonly Int32 originLife;
+        private readonly Int32 originMana;
+
+        private Int32 attack;
+        private Int32 life;
+        private Int32 maxLife;
+        private Int32 mana;
+
         private Card()
         {
-            //TODO;
+
         }
+
         public Card(string def)
         {
             //TODO;
+        }
+
+        public int Initialize()
+        {
+            //TODO;
+            return 0;
         }
     }
 }
